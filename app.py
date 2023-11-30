b37a04c43b896390a7fa4a3dbdb9513c0d936bcc
hanasoka <57451015+hanasoka@users.noreply.github.com>
2023-11-30 16:44:29 +0000

    added some data on app.py

--- app.py
+++ app.py
@@ -2,10 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+ARTICLES = [
+  {
+    "id": 1,
+    "title": "Island Tourism",
+    "author": "John Smith",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+  },
+  {
+    "id": 2,
+    "title": "Fishing Trip",
+    "author": "Jane Doe",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+  },
+  {
+    "id": 3,
+    "title": "Skiing Trip",
+    "author": "Bob Johnson",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+  },
+  {
+    "id": 4,
+    "title": "Hiking Trip",
+    "author": "Sarah Lee",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+  } 
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=ARTICLES, owner="Han Asoka")
 
 
 if __name__ == '__main__':
