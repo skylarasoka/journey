9d55684ed86686a987d4875c160fc265d903cfca
hanasoka <57451015+hanasoka@users.noreply.github.com>
2023-12-03 03:57:36 +0000

    render dynamic data using api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify 
 
 app = Flask(__name__)
 
@@ -32,6 +32,9 @@ ARTICLES = [
 def hello_world():
   return render_template('home.html', jobs=ARTICLES, owner="Han Asoka")
 
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(ARTICLES)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
