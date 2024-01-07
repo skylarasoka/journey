0d089a71f12e1bba093ca9fceb0ebf5e4db14bbf
hanasoka <57451015+hanasoka@users.noreply.github.com>
2024-01-07 13:14:05 +0000

    added new route

--- app.py
+++ app.py
@@ -36,5 +36,13 @@ def hello_world():
 def list_jobs():
   return jsonify(ARTICLES)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
